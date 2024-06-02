85c92652e0856c1245cf6a586ca4747c3e3594de
(Alex)andre Cardoso <100371225+BIGTOST@users.noreply.github.com>
2024-06-02 21:44:54 +0100

    update 2

--- MOOD(GameDesignProject)/Assets/Script/MoveCamara.cs
+++ MOOD(GameDesignProject)/Assets/Script/MoveCamara.cs
@@ -4,7 +4,19 @@ using UnityEngine;
 
 public class MoveCamara : MonoBehaviour
 {
-    [SerializeField]private float speed;
-    private float
-    
+    [SerializeField] private float speed;
+    private float currentPosX,
+                  currentPosZ;
+    private Vector3 velocity = Vector3.zero;
+
+    private void Update()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, currentPosZ), ref velocity, speed);
+    }
+
+    public void MoveToNewRoom(Transform _newRoom)
+    {
+        currentPosX = _newRoom.position.x;
+        currentPosZ = _newRoom.position.z;
+    }
 }
