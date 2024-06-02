b00e3c970c0bc0d9e98979822f51f28a3ae06a35
(Alex)andre Cardoso <100371225+BIGTOST@users.noreply.github.com>
2024-06-02 18:52:38 +0100

    Commit 18:52

---
+++ MOOD(GameDesignProject)/Assets/Script/MoveCamara.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamara : MonoBehaviour
+{
+    [SerializeField]private float speed;
+    private float
+    
+}
