3999e70dd6096a91823f87a0c425ebdeb7b1c0eb
danielasofialf <danielasofialf@gmail.com>
2024-06-03 16:05:53 +0100

    update menu

--- MOOD(GameDesignProject)/Assets/Script/HealthBar.cs
+++ MOOD(GameDesignProject)/Assets/Script/HealthBar.cs
@@ -6,8 +6,23 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     [SerializeField] private Image _healthbarSprite;
-    // Start is called before the first frame update
-    public void UpdateHealthBar(float maxHealth, float currentHealth){
-    _healthbarSprite.fillAmount = currentHealth / maxHealth;
+    [SerializeField] private GameObject gameOverUI; // Arraste o painel de Game Over aqui no Inspetor
+
+    // Atualiza a barra de saúde
+    public void UpdateHealthBar(float maxHealth, float currentHealth)
+    {
+        _healthbarSprite.fillAmount = currentHealth / maxHealth;
+
+        // Verifica se a saúde chegou a zero
+        if (currentHealth <= 0)
+        {
+            ShowGameOverUI();
+        }
+    }
+
+    // Mostra o painel de Game Over
+    private void ShowGameOverUI()
+    {
+        gameOverUI.SetActive(true);
     }
 }
