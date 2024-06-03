aea8499afc0d9e93440d2e40ed2ed1db41eefc23
(Alex)andre Cardoso <100371225+BIGTOST@users.noreply.github.com>
2024-06-03 23:08:10 +0100

    update final menus

---
+++ MOOD(GameDesignProject)/Assets/Script/UIMenuManager.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIMenuManager : MonoBehaviour
+{
+    [SerializeField] private GameObject MainMenu;
+    [SerializeField] private GameObject Options;
+    [SerializeField] private GameObject Creditos;
+
+    void Awake()
+    {
+        Options.SetActive(false);
+        Creditos.SetActive(false);
+        MainMenu.SetActive(true);
+    }
+
+
+    public void OptionsOpen(){
+        Options.SetActive(true);
+        MainMenu.SetActive(false);
+    }
+
+    public void OptionsClose()
+    {
+        Options.SetActive(false);
+        MainMenu.SetActive(true);
+    }
+    
+
+    public void OpenCredits()
+    {
+        Creditos.SetActive(true);
+        MainMenu.SetActive(false);
+    }
+    public void CloseCredits()
+    {
+        Creditos.SetActive(false);
+        MainMenu.SetActive(true);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
