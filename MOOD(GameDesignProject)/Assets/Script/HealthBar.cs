c657343f31b3e25206d87f605bdc9c4fef619c10
(Alex)andre Cardoso <100371225+BIGTOST@users.noreply.github.com>
2024-06-15 18:07:17 +0100

    Update HealthBar.cs

--- MOOD(GameDesignProject)/Assets/Script/HealthBar.cs
+++ MOOD(GameDesignProject)/Assets/Script/HealthBar.cs
@@ -6,13 +6,10 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     [SerializeField] private Image _healthbarSprite;
-    [SerializeField] private GameObject gameOverUI; // Arraste o painel de Game Over aqui no Inspetor
 
     // Atualiza a barra de saúde
     public void UpdateHealthBar(float maxHealth, float currentHealth)
     {
         _healthbarSprite.fillAmount = currentHealth / maxHealth;
-
-        
     }
 }
