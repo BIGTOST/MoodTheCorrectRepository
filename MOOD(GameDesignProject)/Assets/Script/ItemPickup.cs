848a2c846c6a283ebe7c098a5ef0345cb2df3fbd
danielasofialf <danielasofialf@gmail.com>
2024-07-02 02:18:10 +0100

    update_pickupitems

--- MOOD(GameDesignProject)/Assets/Script/ItemPickup.cs
+++ MOOD(GameDesignProject)/Assets/Script/ItemPickup.cs
@@ -1,17 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ItemPickup : MonoBehaviour
 {
-    // Start is called before the first frame update
-    private GameObject item_relampagoInRange;
-    
+    // SerializeField para associar os TextMesh Pro no Unity
+    [SerializeField] private TextMeshProUGUI agilityText;
+    [SerializeField] private TextMeshProUGUI powerText;
+
+    // Contadores
+    private int agilityCount = 0;
+    private int powerCount = 0;
+
+    private GameObject itemInRange;
+
+    void Start()
+    {
+        // Inicializa os textos no início
+        UpdateAgilityText();
+        UpdatePowerText();
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.P))
         {
-            if (item_relampagoInRange != null)
+            if (itemInRange != null)
             {
                 PickUpItem();
             }
@@ -20,26 +35,52 @@ public class ItemPickup : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("item_relampago"))
+        if (other.CompareTag("item_relampago") || other.CompareTag("item_escudo") || other.CompareTag("item_amuleto"))
         {
-            item_relampagoInRange = other.gameObject;
-            Debug.Log("Item in range: " + item_relampagoInRange.name);
+            itemInRange = other.gameObject;
+            Debug.Log("Item in range: " + itemInRange.name);
         }
     }
 
     void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("item_relampago"))
+        if (other.CompareTag("item_relampago") || other.CompareTag("item_escudo") || other.CompareTag("item_amuleto"))
         {
-            item_relampagoInRange = null;
+            itemInRange = null;
             Debug.Log("Item out of range.");
         }
     }
 
     void PickUpItem()
     {
-        Debug.Log("Picked up: " + item_relampagoInRange.name);
-        Destroy(item_relampagoInRange);
-        item_relampagoInRange = null;
+        Debug.Log("Picked up: " + itemInRange.name);
+
+        if (itemInRange.CompareTag("item_relampago"))
+        {
+            agilityCount += 10;
+            UpdateAgilityText();
+        }
+        else if (itemInRange.CompareTag("item_escudo") || itemInRange.CompareTag("item_amuleto"))
+        {
+            powerCount += 10;
+            UpdatePowerText();
+        }
+
+        Destroy(itemInRange);
+        itemInRange = null;
+    }
+
+    // Método para atualizar o TextMesh Pro com o valor atual do contador de agilidade
+    void UpdateAgilityText()
+    {
+        agilityText.text = agilityCount.ToString();
+        Debug.Log("Updated Agility Text: " + agilityText.text);
+    }
+
+    // Método para atualizar o TextMesh Pro com o valor atual do contador de poder
+    void UpdatePowerText()
+    {
+        powerText.text = powerCount.ToString();
+        Debug.Log("Updated Power Text: " + powerText.text);
     }
 }
