5385f8c3e52d7927eead30fbff34a07a97efcddc
danielasofialf <danielasofialf@gmail.com>
2024-07-07 15:47:51 +0100

    Loja

--- MOOD(GameDesignProject)/Assets/Script/ItemPickup.cs
+++ MOOD(GameDesignProject)/Assets/Script/ItemPickup.cs
@@ -34,7 +34,8 @@ public class ItemPickup : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("item_relampago") || other.CompareTag("item_escudo") || other.CompareTag("item_amuleto") || other.CompareTag("item_orbe"))
+        if (other.CompareTag("item_relampago") || other.CompareTag("item_escudo") || other.CompareTag("item_amuleto") || other.CompareTag("item_orbe") ||
+            other.CompareTag("loja_relampago") || other.CompareTag("loja_escudo") || other.CompareTag("loja_amuleto") || other.CompareTag("loja_orbe"))
         {
             itemInRange = other.gameObject;
             Debug.Log("Item in range: " + itemInRange.name);
@@ -43,7 +44,8 @@ public class ItemPickup : MonoBehaviour
 
     void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("item_relampago") || other.CompareTag("item_escudo") || other.CompareTag("item_amuleto") || other.CompareTag("item_orbe"))
+        if (other.CompareTag("item_relampago") || other.CompareTag("item_escudo") || other.CompareTag("item_amuleto") || other.CompareTag("item_orbe") ||
+            other.CompareTag("loja_relampago") || other.CompareTag("loja_escudo") || other.CompareTag("loja_amuleto") || other.CompareTag("loja_orbe"))
         {
             itemInRange = null;
             Debug.Log("Item out of range.");
@@ -54,27 +56,31 @@ public class ItemPickup : MonoBehaviour
     {
         Debug.Log("Picked up: " + itemInRange.name);
 
-        if (itemInRange.CompareTag("item_relampago"))
+        if (itemInRange.CompareTag("item_relampago") || itemInRange.CompareTag("loja_relampago"))
         {
             agilityCount += 10;
             UpdateAgilityText();
             StartCoroutine(ShowPickupMessage("+10 Agility")); 
         }
-        else if (itemInRange.CompareTag("item_escudo") || itemInRange.CompareTag("item_amuleto"))
+        else if (itemInRange.CompareTag("item_escudo") || itemInRange.CompareTag("loja_escudo") ||
+                 itemInRange.CompareTag("item_amuleto") || itemInRange.CompareTag("loja_amuleto"))
         {
             powerCount += 10;
             UpdatePowerText();
             StartCoroutine(ShowPickupMessage("+10 Power")); 
         }
-        else if (itemInRange.CompareTag("item_orbe"))
+        else if (itemInRange.CompareTag("item_orbe") || itemInRange.CompareTag("loja_orbe"))
         {
             Debug.Log("Orbe item picked up, increasing player health.");
             player.IncreaseHealth(10f); 
             StartCoroutine(ShowPickupMessage("+10 Health")); 
         }
 
-        Destroy(itemInRange);
-        itemInRange = null;
+        if (itemInRange.CompareTag("item_relampago") || itemInRange.CompareTag("item_escudo") ||
+            itemInRange.CompareTag("item_amuleto") || itemInRange.CompareTag("item_orbe"))
+        {
+            Destroy(itemInRange);
+        }
     }
 
     void UpdateAgilityText()
